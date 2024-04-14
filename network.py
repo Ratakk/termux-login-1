453908fe1116488e51c97016c7051ac250216918
Ratakk <124754724+Ratakk@users.noreply.github.com>
2024-04-14 10:04:21 -0500

    Beto

--- network.py
+++ network.py
@@ -9,6 +9,6 @@ url = "http://www.google.com"
 timeout = 5.
 try:
     request = requests. get(url, timeout=timeout)
-    print("                \033[1m\033[42m ⚡\033[31mOnline⚡ \033[0m")
+    print("                \033[1m\033[42m ⚡\033[31mConectado⚡ \033[0m")
 except (requests. ConnectionError, requests. Timeout) as exception:
     print("                \033[1m\033[42m ⚡\033[31mOFFLINE⚡ \033[0m")
