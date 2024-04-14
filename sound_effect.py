de656d8f5171c0927a3fe1df534ca04e2dff5035
Ratakk <124754724+Ratakk@users.noreply.github.com>
2024-04-14 09:59:45 -0500

    Beto

--- sound_effect.py
+++ sound_effect.py
@@ -8,7 +8,7 @@ print("\033[1m\033[33m[\033[32m-----Booting-----\033[33m]\033[0m")
 # Condition 
 
 if num == 1:
-    os.system("mpv Access-Granted.mp3")
+    os.system("mpv listo.mp3)
 elif num==2:
     os.system("mpv Jarvis2.mp3")
 else :
