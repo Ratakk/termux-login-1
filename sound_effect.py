a35cc742c7f1a26ef77718eac6db2a8932197e8e
Ratakk <124754724+Ratakk@users.noreply.github.com>
2024-04-14 10:02:24 -0500

    Beto

--- sound_effect.py
+++ sound_effect.py
@@ -3,12 +3,12 @@ import os
 num=random.randint(1,3)
 print(num)
 os.system("clear")
-print("\033[1m\033[33m[\033[32m-----Booting-----\033[33m]\033[0m")
+print("\033[1m\033[33m[\033[32m-----Iniciando-----\033[33m]\033[0m")
 
 # Condition 
 
 if num == 1:
-    os.system("mpv Access-Granted.mp3")
+    os.system("mpv Acceso-concedido.mp3")
 elif num==2:
     os.system("mpv Jarvis2.mp3")
 else :
